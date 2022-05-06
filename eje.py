9d80c3ac6cf5cb3d7925eb803b1d82b24b205ae3
laucadi <lauracaicedo182@gmail.com>
2022-05-06 08:57:08 -0500

    first commit

---
+++ eje.py
@@ -0,0 +1,4 @@
+Nombre=str(input("ingrese nombre: "))
+print(type(Nombre))
+Edad=str(input("ingrese edad: " ))
+print(type(Edad))
---
+++ suma.py
@@ -0,0 +1,9 @@
+def sumarDosNumeros():
+    num1 = float(input("Ingrese el numero 1:"))
+    num2 = float (input("Ingrese el numero 2: "))
+    print("la suma de", int(num1), "+",int(num2), "es igual a: ", int(num1 + num2))
+
+sumarDosNumeros()
+
+
+
